05aa733b77837df7442c10fbcfc7fa9e4ab04ac7
kt1719 <khayle.torres19@imperial.ac.uk>
2024-02-09 15:30:47 +0000

    Improved more of the logic of map gen

--- Assets/Scripts/Utils/Editor/MapGen/MapGenTool.cs
+++ Assets/Scripts/Utils/Editor/MapGen/MapGenTool.cs
@@ -32,18 +32,17 @@ namespace GameTools {
         private class Cell {
             public uint cellId;
             public GameObject cellGameObject;
-            public bool leftPath;
-            public bool rightPath;
-            public bool topPath;
-            public bool bottomPath;
+            public Dictionary<string, bool> pathsAllowed = new Dictionary<string, bool>();
 
             public Cell(uint cellId, GameObject cellGameObject, string pathsAllowedString) {
                 this.cellId = cellId;
                 this.cellGameObject = cellGameObject;
-                this.leftPath = pathsAllowedString[0] == '1';
-                this.rightPath = pathsAllowedString[1] == '1';
-                this.topPath = pathsAllowedString[2] == '1';
-                this.bottomPath = pathsAllowedString[3] == '1';
+                this.pathsAllowed = new Dictionary<string, bool>() {
+                    { "left", pathsAllowedString[0] == '1' },
+                    { "right", pathsAllowedString[1] == '1' },
+                    { "top", pathsAllowedString[2] == '1' },
+                    { "bottom", pathsAllowedString[3] == '1' }
+                };
             }
         }
         // Hashmap of compatible cells for each path
@@ -57,7 +56,8 @@ namespace GameTools {
         // Mapping of cell id to cell object
         private Dictionary<uint, Cell> cellMappings = new Dictionary<uint, Cell>();
         private List<uint> entranceCells = new List<uint>();
-        uint cellId = 0;
+        private uint cellId = 0;
+        private uint mapId = 0;
         private Vector2 mapDimensions = new Vector2(10, 10);
         private Vector2 cellSize = new Vector2(30, 20);
 
@@ -69,6 +69,7 @@ namespace GameTools {
 			EditorGUILayout.PropertyField(cellList, new GUIContent("List of Cells"), true);
             mapDimensions = EditorGUILayout.Vector2Field("Map Dimensions", mapDimensions);
             cellSize = EditorGUILayout.Vector2Field("Cell Size", cellSize);
+            mapId = (uint)EditorGUILayout.DelayedIntField("Map Id", (int)mapId);
 
             if (GUI.changed) {
                 serializedObject.ApplyModifiedProperties();
@@ -79,43 +80,53 @@ namespace GameTools {
             }
 		}
 
-        private void GenerateMap() {
+        private void GenerateMap()
+        {
             Reset();
-            Debug.Log(serializedObject.FindProperty("cellList").arraySize);
+
+            Debug.Log("Generating hashmap...");
+            GenerateHashMap();
 
             Debug.Log("Generating map...");
+            GenerateMapFromHashMap();
+        }
+
+        private void GenerateHashMap()
+        {
             // Print the game object of the cell
             SerializedProperty serializedProperty = serializedObject.FindProperty("cellList");
             // Loop through the list of cells
-            for (int i = 0; i < serializedProperty.arraySize; i++) {
+            for (int i = 0; i < serializedProperty.arraySize; i++)
+            {
                 GameObject cell = serializedProperty.GetArrayElementAtIndex(i).objectReferenceValue as GameObject;
                 // Get the SuperCustomProperties component
                 SuperCustomProperties superCustomProperties = cell.GetComponent<SuperCustomProperties>();
                 // Get the custom property "paths_allowed"
                 CustomProperty cellPathProperty;
-                if (superCustomProperties.TryGetCustomProperty("entrance", out cellPathProperty)) {
+                if (superCustomProperties.TryGetCustomProperty("entrance", out cellPathProperty))
+                {
                     // Parse the value of the custom property (format is $,$,$,$ where each $ is a boolean - top, bottom, left, right)
                     UpdateEntranceList(cellPathProperty.m_Value, cell, cellId);
                 }
-                if (superCustomProperties.TryGetCustomProperty("paths_allowed", out cellPathProperty)) {
+                if (superCustomProperties.TryGetCustomProperty("paths_allowed", out cellPathProperty))
+                {
                     // Parse the value of the custom property (format is $,$,$,$ where each $ is a boolean - top, bottom, left, right)
                     UpdatePathsHashMap(cellPathProperty.m_Value, cell, cellId);
                 }
                 cellId += 1;
             }
             // Print the hashmaps generated
-            foreach (KeyValuePair<string, List<uint>> entry in compatibleCells) {
+            foreach (KeyValuePair<string, List<uint>> entry in compatibleCells)
+            {
                 Debug.Log(entry.Key + ": " + string.Join(",", entry.Value));
             }
             // print the name of the key and the value of the key
-            foreach (KeyValuePair<uint, Cell> entry in cellMappings) {
+            foreach (KeyValuePair<uint, Cell> entry in cellMappings)
+            {
                 Debug.Log(entry.Key + ": " + entry.Value.cellGameObject.name);
             }
             // print the entrance cells
             Debug.Log("Entrance cells: " + string.Join(",", entranceCells) + ". Name: " + cellMappings[entranceCells[0]].cellGameObject.name);
-
-            // Generate the map
-            GenerateMapFromHashMap();
         }
 
         private void UpdateEntranceList(string m_Value, GameObject cell, uint cellId)
@@ -127,79 +138,126 @@ namespace GameTools {
 
         private void GenerateMapFromHashMap()
         {
-            bool HasBeenPlaced(Vector2 currPosition, string path, HashSet<Vector2> placedCells) {
-                switch (path) {
-                    case "top":
-                        return placedCells.Contains(new Vector2(currPosition.x, currPosition.y + cellSize.y));
-                    case "bottom":
-                        return placedCells.Contains(new Vector2(currPosition.x, currPosition.y - cellSize.y));
-                    case "left":
-                        return placedCells.Contains(new Vector2(currPosition.x - cellSize.x, currPosition.y));
-                    case "right":
-                        return placedCells.Contains(new Vector2(currPosition.x + cellSize.x, currPosition.y));
-                }
-                return false;
-            }
             // hashset of cell positions that have been placed
             HashSet<Vector2> placedCells = new HashSet<Vector2>();
             System.Random rnd = new System.Random();
             Vector2 currMapSize = new Vector2(1, 1); // This includes the entrance cell
-            Vector2 currPos = new Vector2(0, 0);
+            Vector2 coordinatePosition = new Vector2(0, 0);
             Vector2 maxPos = new Vector2(0, 0);
             Vector2 minPos = new Vector2(0, 0);
 
+            // Create a game object to hold the map
+            GameObject map = new GameObject("Map_" + mapId);
             // Generate the map
             // 1. Pick a random cell from the list of cells
             uint entranceCellId = entranceCells[rnd.Next(entranceCells.Count)];
             Cell entranceCell = cellMappings[entranceCellId];
-            PlaceCell(entranceCell, new Vector2(0, 0));
+            Cell currCell = entranceCell;
+            PlaceCell(entranceCell, new Vector2(0, 0), map);
             placedCells.Add(new Vector2(0, 0));
 
             string [] paths = new string[] { "top", "bottom", "left", "right" };
-            int x = 0;
-            while (currMapSize.x < mapDimensions.x && currMapSize.y < mapDimensions.y) {
+            while (currMapSize.x < mapDimensions.x && currMapSize.y < mapDimensions.y)
+            {
+                string randomPath = GenerateRandomStringPath(placedCells, rnd, coordinatePosition, currCell, paths);
+                // 3. Pick a random cell from the list of compatible cells for the path
+                Cell randomCell = GetCompatibleCell(rnd, randomPath);
+                // 4. Place the cell
+                coordinatePosition = PlaceCell(randomCell, coordinatePosition, map, randomPath);
+                placedCells.Add(coordinatePosition);
+                currMapSize = CalcualteNewMapSize(coordinatePosition, ref maxPos, ref minPos);
+            }
+            mapId++;
+
+            /////////// HELPER FUNCTIONS ///////////
+            
+            Vector2 PlaceCell(Cell cell, Vector2 coordinatePosition, GameObject parent, string pathString = "") {
+                switch (pathString)
+                {
+                    case "top":
+                        coordinatePosition.y += 1;
+                        break;
+                    case "bottom":
+                        coordinatePosition.y -= 1;
+                        break;
+                    case "left":
+                        coordinatePosition.x -= 1;
+                        break;
+                    case "right":
+                        coordinatePosition.x += 1;
+                        break;
+                    // If it's '' then it's the entrance cell
+                }
+
+                // Center the cell since the pivot is at the top left
+                Vector2 adjustedPos = new Vector2(coordinatePosition.x - (1/2), coordinatePosition.y + (1/2));
+                Vector2 position = new Vector2(adjustedPos.x * cellSize.x, adjustedPos.y * cellSize.y);
+                GameObject cellInstantiation = Instantiate(cell.cellGameObject, position, Quaternion.identity);
+                cellInstantiation.transform.parent = parent ? parent.transform : GameObject.Find("Map").transform;
+                return coordinatePosition;
+            }
+
+            bool HasBeenPlaced(Vector2 coordinatePosition, string path, HashSet<Vector2> placedCells) {
+                switch (path) {
+                    case "top":
+                        return placedCells.Contains(new Vector2(coordinatePosition.x, coordinatePosition.y + 1));
+                    case "bottom":
+                        return placedCells.Contains(new Vector2(coordinatePosition.x, coordinatePosition.y - 1));
+                    case "left":
+                        return placedCells.Contains(new Vector2(coordinatePosition.x - 1, coordinatePosition.y));
+                    case "right":
+                        return placedCells.Contains(new Vector2(coordinatePosition.x + 1, coordinatePosition.y));
+                }
+                return false;
+            }
+
+            Cell GetCompatibleCell(System.Random rnd, string path)
+            {
+                // We want to get the opposite path since we are looking for the next cell e.g., the if the exit is on the top, we want the entrance to be on the bottom
+                uint randomCellId = 0;
+                switch (path)
+                {
+                    case "top":
+                        randomCellId = compatibleCells["bottom"][rnd.Next(compatibleCells["bottom"].Count)];
+                        break;
+                    case "bottom":
+                        randomCellId = compatibleCells["top"][rnd.Next(compatibleCells["top"].Count)];
+                        break;
+                    case "left":
+                        randomCellId = compatibleCells["right"][rnd.Next(compatibleCells["right"].Count)];
+                        break;
+                    case "right":
+                        randomCellId = compatibleCells["left"][rnd.Next(compatibleCells["left"].Count)];
+                        break;
+                }
+                return cellMappings[randomCellId];
+            }
+
+            string GenerateRandomStringPath(HashSet<Vector2> placedCells, System.Random rnd, Vector2 currPos, Cell currCell, string[] paths)
+            {
                 // 2. Pick a random path from the list of paths
                 string randomPath = paths[rnd.Next(paths.Length)];
                 // Check if this path has been visited if so then regenerate another path
-                while (HasBeenPlaced(currPos, randomPath, placedCells)) {
-                    randomPath = paths[rnd.Next(paths.Length)];
-                }
-                // 3. Pick a random cell from the list of compatible cells for the path
-                List<uint> compatibleCellsList = compatibleCells[randomPath];
-                uint randomCellId = compatibleCellsList[rnd.Next(compatibleCellsList.Count)];
-                Cell randomCell = cellMappings[randomCellId];
-                // 4. Place the cell
-                switch (randomPath) {
-                    case "top":
-                        currPos.y += 1;
+                for (int i = 0; i < paths.Length; i++)
+                {
+                    if (!HasBeenPlaced(currPos, randomPath, placedCells) && currCell.pathsAllowed[randomPath])
+                    {
+                        randomPath = paths[rnd.Next(paths.Length)];
                         break;
-                    case "bottom":
-                        currPos.y -= 1;
-                        break;
-                    case "left":
-                        currPos.x -= 1;
-                        break;
-                    case "right":
-                        currPos.x += 1;
-                        break;
+                    }
                 }
-                if (!placedCells.Contains(currPos)) {
-                    PlaceCell(randomCell, currPos);
-                    placedCells.Add(currPos);
-                    maxPos = new Vector2(Math.Max(maxPos.x, currPos.x), Math.Max(maxPos.y, currPos.y));
-                    minPos = new Vector2(Math.Min(minPos.x, currPos.x), Math.Min(minPos.y, currPos.y));
-                    currMapSize = new Vector2(maxPos.x - minPos.x, maxPos.y - minPos.y);
-                }
-                // 5. If no compatible cells are found, restart the process (NO)
-                if (x > 100) {
-                    Debug.Log("Break");
-                    break;
-                }
-                x += 1;
-            }
-            // 6. If the map is generated, return the map (NO)
 
-            // 7. If the map is not generated, return null (NO)
+                return randomPath;
+            }
+
+            Vector2 CalcualteNewMapSize(Vector2 currPos, ref Vector2 maxPos, ref Vector2 minPos)
+            {
+                Vector2 currMapSize;
+                maxPos = new Vector2(Math.Max(maxPos.x, currPos.x), Math.Max(maxPos.y, currPos.y));
+                minPos = new Vector2(Math.Min(minPos.x, currPos.x), Math.Min(minPos.y, currPos.y));
+                currMapSize = new Vector2(maxPos.x - minPos.x, maxPos.y - minPos.y);
+                return currMapSize;
+            }
         }
 
         private void UpdatePathsHashMap(string pathsAllowedString, GameObject cellGameObject, uint cellId) {
@@ -233,14 +291,5 @@ namespace GameTools {
             entranceCells.Clear();
             cellId = 0;
         }
-	
-        private void PlaceCell(Cell cell, Vector2 coordinatePosition) {
-            // Center the cell since the pivot is at the top left
-            coordinatePosition.x -= 1 / 2;
-            coordinatePosition.y += 1 / 2;
-            Vector2 position = new Vector2(coordinatePosition.x * cellSize.x, coordinatePosition.y * cellSize.y);
-            Instantiate(cell.cellGameObject, position, Quaternion.identity);
-        }
-
     }
 }
